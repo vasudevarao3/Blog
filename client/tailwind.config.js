d149b0d0c08009a2ba90034da16405f5babbd178
Bharathmule <Bharathmule427@gmail.com>
2024-02-09 11:02:43 +0530

    Created Header

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,9 +3,11 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
+    // "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
