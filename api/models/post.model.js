811465a61d8346f9fc474b498ad2784d6d652861
vassudevarao3 <vasudevarao3@gmail.com>
2024-02-19 21:15:58 +0530

    Add create post API route

---
+++ api/models/post.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+    {
+        userId: {
+            type: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        image: {
+            type: String,
+            default: "https://wallpapers.com/images/featured-full/funny-monkey-pictures-gapupnyhalncb5nx.jpg",
+        },
+        category: {
+            type: String,
+            default: 'uncategorized',
+        },
+        slug: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+    },{ timestamps: true}
+);
+
+const Post = mongoose.model('post', postSchema);
+
+export default Post;
