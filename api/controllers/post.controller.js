d3a0df374802f4cfbd94123a4a7452662e5236cd
vassudevarao3 <vasudevarao3@gmail.com>
2024-02-20 20:40:04 +0530

    create get posts API route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -8,7 +8,7 @@ export const create = async(req, res, next) => {
     if(!req.body.title || !req.body.content) {
         return next(errorHandler(400, 'Please provide all the required fields'))
     }
-    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9]/g, '-');
+    const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9-]/g, '');
     const newPost = new Post ({
         ...req.body, slug, userId: req.user.id,
     });
@@ -18,4 +18,42 @@ export const create = async(req, res, next) => {
     } catch (error) {
         next(error);
     }
+};
+
+export const getposts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex)  || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === 'asc' ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { category: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && { 
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: 'i'} },
+          { content: { $regex: req.query.searchTerm, $options: 'i'} },
+        ],
+      }),
+  }).sort({updatedAt: sortDirection }).skip(startIndex).limit(limit);
+
+  const totalPosts = await Post.countDocuments();
+
+  const now = new Date();
+  
+  const oneMonthAgo = new Date(
+    now.getFullYear(),
+    now.getMonth() - 1,
+    now.getDate()
+  );
+  
+  const lastMonthPosts = await Post.countDocuments({
+    createdAt: { $gte: oneMonthAgo },
+  });
+
+  res.status(200).json({ posts, totalPosts, lastMonthPosts});
+  } catch (error) {
+    next(error);
+  }
 };
