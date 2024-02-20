4701dca3308d3b649d2f1b299a99c741269b3f2a
vassudevarao3 <vasudevarao3@gmail.com>
2024-02-20 22:06:47 +0530

    show user posts inside the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
-import DashPost from '../components/DashPost';
+import DashPosts from '../components/DashPosts';
 
 export default function Dashboard() {
   const location = useLocation()
@@ -24,7 +24,7 @@ export default function Dashboard() {
         {/* Profile */}
         {tab === 'profile' && <DashProfile />}
         {/* Posts */}
-        {tab === 'posts' && <DashPost />}
+        {tab === 'posts' && <DashPosts />}
     </div>
   );
 }
