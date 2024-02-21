e8372f1319ba4ae9128d7d228cb14e60c4c71945
vassudevarao3 <vasudevarao3@gmail.com>
2024-02-21 20:48:23 +0530

    Complete Post Page functionality

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -79,26 +79,27 @@ export default function UpdatePost() {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await fetch(`/api/post/updatepost/${formData._id}/${currentUser._id}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
-            const data = await res.json();
-            if(!res.ok){
-                setPublishError(data.message);
-                return;
-            }
-            if(res.ok){
-                setPublishError(null);
-                navigate(`/post/${data.slug}`);
-            }
+          const res = await fetch(`/api/post/updatepost/${postId}/${currentUser._id}`, {
+            method: 'PUT',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          });
+          const data = await res.json();
+          if (!res.ok) {
+            setPublishError(data.message);
+            return;
+          }
+    
+          if (res.ok) {
+            setPublishError(null);
+            navigate(`/post/${data.slug}`);
+          }
         } catch (error) {
-            setPublishError('Something went wrong...');
+          setPublishError('Something went wrong');
         }
-    };
+      };
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen'>
         <h1 className='text-center text-3xl my-7 font-semibold'>Update Post</h1>
@@ -132,6 +133,7 @@ export default function UpdatePost() {
             {formData.image && (
                 <img src={formData.image} alt='upload' className='w-full h-72 object-cover' />
             )}
+            {/* To style the content inside the ReactQuill use manual CSS in index.jsx */}
             <ReactQuill theme='snow' value={formData.content} placeholder='Write something...' className='h-72 mb-12' required onChange={(value) => setFormData({...formData, content: value})}/>
             <Button type='submit' gradientDuoTone='purpleToPink'>
                 Update Post
