2ea00ed105b264e06d6f96582c567eaafdbe4aeb
vassudevarao3 <vasudevarao3@gmail.com>
2024-02-21 21:25:23 +0530

    Add call to Action to the Post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,26 @@
+import { Button } from 'flowbite-react'
+import React from 'react'
+
+export default function CallToAction() {
+  return (
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
+        <div className='flex-1 justify-center flex flex-col'>
+            <h2 className='text-2xl'>
+                Want to learn more about Gorillas?
+            </h2>
+            <p className='text-gray-500 my-2'>
+                Checkout these resources with Types of gorillas
+            </p>
+            <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
+                <a href='www.google.com' target='_blank' rel='noopener noreferrer'>
+                    Siggu Gorilla
+                </a>
+            </Button>
+        </div>
+        <div className='p-7 flex-1'>
+            <img src='https://media.istockphoto.com/id/489669192/photo/funny-gorilla-with-sunglasses-celebrating-party-by-blowing-a-horn.jpg?s=612x612&w=0&k=20&c=Z3nPbqmeXYO8kKrhlbza2cS3-I1oaLvi-fbwFkcP8Mk='
+                alt='Gorrilla' />
+        </div>
+    </div>
+  )
+}
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import CallToAction from '../components/CallToAction';
 
 export default function PostPage() {
     const { postSlug } = useParams();
@@ -56,6 +57,9 @@ export default function PostPage() {
             <span>{post && (post.content.length/1000).toFixed(0)} mins reads</span>
         </div>
         <div className='p-3 max-w-2xl mx-auto w-full text-justify post-content' dangerouslySetInnerHTML={{__html: post && post.content}}>
+        </div>
+        <div className='max-w-4xl mx-auto w-full'>
+            <CallToAction />
         </div>
     </main>
   )
