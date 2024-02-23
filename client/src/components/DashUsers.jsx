c02b8433583b0cbd3efc20f011f5b635d32ab1d2
vassudevarao3 <vasudevarao3@gmail.com>
2024-02-23 19:22:11 +0530

    Add like functionality to the comment

--- client/src/components/DashUsers.jsx
+++ client/src/components/DashUsers.jsx
@@ -13,7 +13,7 @@ export default function DashUsers() {
   useEffect( () => {
     const fetchUsers = async () => {
       try {
-        const res = await fetch(`api/user/getusers`);
+        const res = await fetch(`/api/user/getusers`);
         const data = await res.json();
         if(res.ok){
           setUsers(data.users);
