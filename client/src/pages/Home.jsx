28e3cf4615ab2807a6d1b8fa3a421b3297f321b2
vassudevarao3 <vasudevarao3@gmail.com>
2024-02-24 20:47:03 +0530

    Add Complete Home Page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
+import CallToAction from '../components/CallToAction';
+import PostCard from '../components/PostCard';
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch('/api/post/getPosts');
+      const data = await res.json();
+      setPosts(data.posts);
+    }
+    fetchPosts();
+  },[])
   return (
-    <div>Home</div>
+    <div>
+      <div className='flex flex-col gap-6 md:p-28 p-7 max-w-6xl mx-auto'>
+        <h1 className='text-3xl font-bold lg:text-6xl'>Welcome to my Blog</h1>
+        <p className='text-gray-500 text-xs sm:text-sm'> Here you will find various articles on varios topics such as Kothi man, Bat Man, Ant man and Pilli Boy</p>
+        <Link to='/search' className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'>
+          View Posts
+        </Link>
+      </div>
+      <div className='p-3 bg-amber-100 dark:bg-slate-700'>
+        <CallToAction/>
+      </div>
+      <div className='max-w-6xl mx-auto md:px-20 p-3 flex flex-col gap-8 py-7'>
+        {
+          posts && posts.length > 0 && (
+            <div className='flex flex-col gap-6'>
+              <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+              <div className=' flex flex-wrap gap-3'>
+                {posts.map((post) => (
+                  <PostCard key={post._id} post={post} />
+                ))}
+              </div>
+              <Link to={'/search'} className='text-lg text-teal-500 hover:underline text-center font-semibold'>
+                View all posts
+              </Link>
+            </div>
+          )
+        }
+      </div>
+    </div>
   )
 }
