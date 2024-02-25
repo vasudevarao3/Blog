c0caf3f1b60e2e5cb0a7c39b14a65bededdd608c
vassudevarao3 <vasudevarao3@gmail.com>
2024-02-25 12:35:37 +0530

    Complete About and Project Pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,7 +2,31 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>about</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <h1 className='text-3xl font font-semibold text-center my-7'>About</h1>
+        <div className='text-md text-gray-500 flex flex-col gap-6'>
+          <p>
+            The sound of scratching and clawing beside his ear woke Mustapha Riat to be confronted with a sight that he will never forget.
+            A chimpanzee, its arm outstretched, was leaning through the window of his ground-floor bedroom and had grabbed his mobile phone from the bedside table.
+            "Its head and chest were leaning right inside the window," said Mr Riat, 43, a language teacher, yesterday.
+            "It was right there in front of me. It's not the first time I have seen a chimp - it was big and black and hairy with a broad chest and a distinctive shaped head.
+          </p>
+          <p>
+            "I jumped out of bed and rushed to slam the window but it was so fast. It pulled back its arm and was gone."
+            Mr Riat, who had left his window open to allow his three cats to go in and out overnight, telephoned the police and reported that he had been burgled by a chimpanzee.
+            He then went to look for the culprit which he believes to be a tame chimp trained to steal by an unscrupulous owner. But the Sunday morning thief had gone, presumably disappearing across the road from Mr Riat's flat on Gore Road, Hackney, east London, into the relative safety of the trees and bushes of Victoria Park.
+            Mr Riat, a Muslim who emphasised that he had never touched alcohol in his life, said: "I ran into the park and asked dog-walkers if they had seen a chimpanzee or a man with a chimpanzee. But no one had seen anything."
+          </p>
+          <p>
+            Police officers were on the scene quickly having been called to a similar burglary less than an hour before just 20 doors from Mr Riat's home. Again a ground-floor flat had been burgled. A watch was stolen from the mantlepiece in Gina Davidson's home but the raider failed in an attempt to take her stereo system and DVD player, which had been dropped on the settee below the open window. Miss Davidson, 23, was sceptical about the idea that a chimpanzee might have been trained to carry out the theft. "I suppose it's possible but it seems rather far-fetched - it would have to have been a pretty intelligent monkey to unplug the stereo and carry it over to the window," she said.
+            Dr Desmond Morris, author of The Naked Ape, shared Miss Davidson's doubts over the identity of the thief.
+            "If you had a cunning thief with a chimpanzee that came in and stole small objects, that would require very specialised training," he said.
+            "I don't think it's an impossibility that a chimp could be encouraged to reach in and grab something, but it's highly unlikely. It is much more likely to have been a person wearing a chimp mask." The criminal potential of the ape world has been explored in literature and film for many years.
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
